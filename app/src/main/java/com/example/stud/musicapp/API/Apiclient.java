07b80e84aab0413fd316d836a71a387e240fd570
Andrii Aleksandrov <alexandrovandrij@gmail.com>
2018-04-19 18:41:54 +0200

    Pobieranie danych

--- app/src/main/java/com/example/stud/musicapp/API/Apiclient.java
+++ app/src/main/java/com/example/stud/musicapp/API/Apiclient.java
@@ -1,5 +1,12 @@
 package com.example.stud.musicapp.API;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface Apiclient {
 
+    @GET( "trending.php" )
+    Call<TrendingList> getTrendingList(@Query ( "country" ) String country, @Query ( "type" )
+            String type, @Query( "format" ) String format);
 }
--- app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsActivity.java
+++ app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsActivity.java
@@ -2,8 +2,17 @@ package com.example.stud.musicapp.topsongs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.example.stud.musicapp.API.Apiservice;
+import com.example.stud.musicapp.API.TrendingList;
 import com.example.stud.musicapp.R;
+import com.google.gson.Gson;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class TopSongsActivity extends AppCompatActivity {
 
@@ -13,6 +22,25 @@ public class TopSongsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_top_songs);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Call<TrendingList> trendingListCall = Apiservice.getService().getTrendingList("us","itunes","singles");
+        trendingListCall.enqueue(new Callback<TrendingList>() {
+            @Override
+            public void onResponse(Call<TrendingList> call, Response<TrendingList> response) {
+                TrendingList trendingList = response.body();
+
+                Toast.makeText(TopSongsActivity.this,"elemtow: "+trendingList.trending.size(), Toast. LENGTH_SHORT ).show();
+                Log.d ( "TAG" , new Gson().toJson(trendingList));
+
+            }
+
+            @Override
+            public void onFailure(Call<TrendingList> call, Throwable t) {
+
+                Toast.makeText(TopSongsActivity.this,"blad pobierania danych"+t.getLocalizedMessage(), Toast. LENGTH_SHORT ).show();
+
+            }
+        });
 
     }
     @Override
